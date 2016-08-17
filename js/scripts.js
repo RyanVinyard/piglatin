c921abdd3fe809a6c078fe726317936f0316a7c6
Addison Nishijima and Ryan Vinyard <addison@>
2016-08-17 14:26:59 -0700

    allow translation of lower case words beginning with consonants into piglatin

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,35 @@ $(document).ready(function() {
   event.preventDefault();
 
   var userInput = $("input#phrase").val();
-  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+  var inputArray = userInput.split("");
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
 
   if (userInput.length === 1) {
      userInput = (userInput + "ay");
    };
 
-  if (userInput[0] != vowels) {
-    var substring = userInput.substring(0, 1);
+  // if (userInput[0] != vowels) {
+  //   alert("works");
+  //   var substring = userInput.substring(0, 1);
 
-    userInput = (userInput.slice(1) + substring + "ay");
+
+  //   userInput = (userInput.slice(1) + substring + "ay");
+  // };
+
+  for (var i = 0; i < vowels.length; i++) {
+    for (var n = 0; n < userInput.length; n++) {
+
+
+      if (userInput[n] === vowels[i]) {
+        var slice = userInput.slice(0,n);
+        var input = userInput.slice(n,100000);
+        userInput = (input + slice + "ay");
+        console.log(slice);
+        console.log(input);
+    };
   };
+};
   // }
   //
   // for (i = 0; i = userInput.length - 1; i++) {
@@ -22,7 +40,10 @@ $(document).ready(function() {
 
 
 
-  $("#result").append(userInput);
+  $("#result").text(userInput);
 
   });
 });
+
+
+//var inputArray = userInput.split("");
